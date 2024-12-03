c845d1ce184e0ddd0c6362c99889046f4c417ac5
François Costa <frankost.costa@gmail.com>
2024-12-03 10:36:44 +0100

    tmp changes

--- run_microbenchmarks.sh
+++ run_microbenchmarks.sh
@@ -1,15 +1,23 @@
-# TODO: Run coremark pro remotely
+########################
+# CPU Microbenchmark
+########################
 
 cd coremark-pro
 make TARGET=linux64 XCMD='-c4' certify-all > "cpu_benchmark.txt"
 cd ..;
 
-# TODO: Run iozone remotely
+########################
+# filesystem Microbenchmark
+########################
+
 cd keystone-iozone
 iozone -a > "fs_benchmark.txt"
 cd ..
 
-# TODO: Run netperf directly from the computer with a remote address
+########################
+# Network Microbenchmark
+########################
+
 cd netperf
 # Start the server 
 netserver
