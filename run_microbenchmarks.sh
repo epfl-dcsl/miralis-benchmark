0f51948805a70fb590ad839349958fe172199c78
François Costa <frankost.costa@gmail.com>
2024-12-07 14:46:09 +0100

    improvement of the scripts

--- run_microbenchmarks.sh
+++ run_microbenchmarks.sh
@@ -35,21 +35,31 @@ create_folder_if_not_exists "results/netperf"
 # CPU Microbenchmark
 ########################
 
+echo "Running CPU Microbenchmark [Coremarkpro]"
+
 cd coremark-pro
 make TARGET=linux64 XCMD='-c4' certify-all > "../results/coremarkpro/$1.txt"
 cd ..;
+
+echo "Done with CPU microbenchmark"
 
 ########################
 # filesystem Microbenchmark
 ########################
 
+cho "Running filesystem microbenchmark [Filesystem]"
+
 cd keystone-iozone
 ./iozone -a > "../results/iozone/$1.txt"
 cd ..
 
+echo "Done with disk microbenchmark"
+
 ########################
 # Network Microbenchmark
 ########################
+
+cho "Running network microbenchmark [netperf]"
 
 cd netperf
 # Start the server 
@@ -59,3 +69,5 @@ netperf -H 127.0.0.1 -t TCP_STREAM  > "results/netperf/$1\_tcp.txt"
 netperf -H 127.0.0.1 -t UDP_STREAM  > "results/netperf/$1\_udp.txt"
 netperf -H 127.0.0.1 -t TCP_RR      > "results/netperf/$1\_rtt.txt"
 cd ..
+
+echo "Done with network microbenchmark"
