c942dc9421dbf891dd170bbacfa38853283a9695
François Costa <frankost.costa@gmail.com>
2024-12-08 00:50:00 +0100

    compilation microbenchmarks, first part

---
+++ benchmark_comp_redis.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <arg1>"
+    echo "Error: Please give the output name"
+    exit 1
+fi
+
+function install_redis() {
+
+    # First delete the repository
+    rm -rf redis
+
+    # Clone the Redis repository
+    git clone https://github.com/redis/redis > /dev/null
+
+    # Navigate to the Redis directory
+    cd redis
+
+    # Run 'make' and capture its timing and output in 'output.txt'
+    (time ls) 2>> "../$1"
+
+    cd ..
+}
+
+echo "" > $1
+
+for i in {0..1} 
+do
+    install_redis $1
+done
+
+python3 redis.py
