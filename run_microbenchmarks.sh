942822eaa89b53375051de7453faa1fd88b2edbc
François Costa <frankost.costa@gmail.com>
2024-12-07 15:08:44 +0100

    tmp changes

--- run_microbenchmarks.sh
+++ run_microbenchmarks.sh
@@ -68,9 +68,9 @@ cd netperf
 # Start the server 
 netserver
 # Benchmark the server
-netperf -H 127.0.0.1 -t TCP_STREAM  > "results/netperf/$1\_tcp.txt"
-netperf -H 127.0.0.1 -t UDP_STREAM  > "results/netperf/$1\_udp.txt"
-netperf -H 127.0.0.1 -t TCP_RR      > "results/netperf/$1\_rtt.txt"
+netperf -H 127.0.0.1 -t TCP_STREAM  > "results/netperf/$1_tcp.txt"
+netperf -H 127.0.0.1 -t UDP_STREAM  > "results/netperf/$1_udp.txt"
+netperf -H 127.0.0.1 -t TCP_RR      > "results/netperf/$1_rtt.txt"
 cd ..
 
 echo "Done with network microbenchmark"
