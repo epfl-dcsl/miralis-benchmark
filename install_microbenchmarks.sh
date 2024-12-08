f6ecdadeed840af577e7f061acfd357850f20ce6
François Costa <frankost.costa@gmail.com>
2024-12-08 14:11:21 +0100

    tmp changes

--- install_microbenchmarks.sh
+++ install_microbenchmarks.sh
@@ -13,12 +13,12 @@ sudo apt-get update -y
 sudo apt-get install make gcc xz-utils bzip2 -y
 sudo apt-get install gcc -y
 
-# CPU Microbenchmark
-# git clone https://github.com/eembc/coremark-pro
-# 
-# cd coremark-pro
-# make TARGET=linux64 build
-# cd ..
+CPU Microbenchmark
+git clone https://github.com/eembc/coremark-pro
+
+cd coremark-pro
+make TARGET=linux64 build
+cd ..
 
 ########################
 # Filesystem Microbenchmark
@@ -30,42 +30,35 @@ cd keystone-iozone
 git clone https://github.com/richfelker/musl-cross-make
 cd musl-cross-make
 
-
-echo "DONE 1"
 make -j$(nproc) TARGET=riscv64-linux-musl
-echo "DONE 2"
 make install TARGET=riscv64-linux-musl
 
+cd ..
 
-echo "DONE"
+git checkout 1378a4fb920e8177a2293c4600ab494ab51de6b8
+CCRV=musl-cross-make/output/bin/riscv64-linux-musl-gcc make keystone
 
-# cd ..
-# 
-# git checkout 1378a4fb920e8177a2293c4600ab494ab51de6b8
-# CCRV=musl-cross-make/output/bin/riscv64-linux-musl-gcc make keystone
-# 
-# cd ..
-# 
-# ########################
-# # Network Microbenchmark
-# ########################
-# 
-# sudo apt install automake autoconf texinfo -y
-# 
-# git clone https://github.com/HewlettPackard/netperf/
-# cd netperf
-# 
-# # Replace confiuration files - files are too old for riscv
-# rm config.guess
-# rm config.sub
-# wget -O config.guess http://git.savannah.gnu.org/cgit/config.git/plain/config.guess
-# wget -O config.sub http://git.savannah.gnu.org/cgit/config.git/plain/config.sub
-# 
-# # Install
-# ./autogen.sh
-# ./configure
-# make CFLAGS="-fcommon"
-# sudo make install
-# 
-# cd ..
-# 
+cd ..
+
+########################
+# Network Microbenchmark
+########################
+
+sudo apt install automake autoconf texinfo -y
+
+git clone https://github.com/HewlettPackard/netperf/
+cd netperf
+
+# Replace confiuration files - files are too old for riscv
+rm config.guess
+rm config.sub
+wget -O config.guess http://git.savannah.gnu.org/cgit/config.git/plain/config.guess
+wget -O config.sub http://git.savannah.gnu.org/cgit/config.git/plain/config.sub
+
+# Install
+./autogen.sh
+./configure
+make CFLAGS="-fcommon"
+sudo make install
+
+cd ..
