ddba90756bc56934903c4b450b23a75001921031
François Costa <frankost.costa@gmail.com>
2024-12-08 13:51:55 +0100

    tmp cahnges

--- install_microbenchmarks.sh
+++ install_microbenchmarks.sh
@@ -30,35 +30,42 @@ cd keystone-iozone
 git clone https://github.com/richfelker/musl-cross-make
 cd musl-cross-make
 
+
+echo "DONE 1"
 make -j$(nproc) TARGET=riscv64-linux-musl
+echo "DONE 2"
 make install TARGET=riscv64-linux-musl
 
-cd ..
 
-git checkout 1378a4fb920e8177a2293c4600ab494ab51de6b8
-CCRV=musl-cross-make/output/bin/riscv64-linux-musl-gcc make keystone
+echo "DONE"
 
-cd ..
-
-########################
-# Network Microbenchmark
-########################
-
-sudo apt install automake autoconf texinfo -y
-
-git clone https://github.com/HewlettPackard/netperf/
-cd netperf
-
-# Replace confiuration files - files are too old for riscv
-rm config.guess
-rm config.sub
-wget -O config.guess http://git.savannah.gnu.org/cgit/config.git/plain/config.guess
-wget -O config.sub http://git.savannah.gnu.org/cgit/config.git/plain/config.sub
-
-# Install
-./autogen.sh
-./configure
-make CFLAGS="-fcommon"
-sudo make install
-
-cd ..
+# cd ..
+# 
+# git checkout 1378a4fb920e8177a2293c4600ab494ab51de6b8
+# CCRV=musl-cross-make/output/bin/riscv64-linux-musl-gcc make keystone
+# 
+# cd ..
+# 
+# ########################
+# # Network Microbenchmark
+# ########################
+# 
+# sudo apt install automake autoconf texinfo -y
+# 
+# git clone https://github.com/HewlettPackard/netperf/
+# cd netperf
+# 
+# # Replace confiuration files - files are too old for riscv
+# rm config.guess
+# rm config.sub
+# wget -O config.guess http://git.savannah.gnu.org/cgit/config.git/plain/config.guess
+# wget -O config.sub http://git.savannah.gnu.org/cgit/config.git/plain/config.sub
+# 
+# # Install
+# ./autogen.sh
+# ./configure
+# make CFLAGS="-fcommon"
+# sudo make install
+# 
+# cd ..
+# 
