d444168180a631daa76ab73b6b06a7b409bc987f
François Costa <frankost.costa@gmail.com>
2024-12-08 15:39:09 +0100

    tmp changes

--- run_kv_workload.sh
+++ run_kv_workload.sh
@@ -13,8 +13,8 @@ if [ "$#" -ne 1 ]; then
 fi
 
 # We only allow three kind of benchmark types
-if ! [[ "$1" == "board" || "$1" == "miralis" || "$1" == "prote" ]]; then
-    echo "Error: Invalid argument. Allowed values are 'board', 'miralis', or 'prote'."
+if ! [[ "$1" == "board" || "$1" == "miralis" || "$1" == "protect" ]]; then
+    echo "Error: Invalid argument. Allowed values are 'board', 'miralis', or 'protect'."
     exit 1
 fi
 
@@ -23,11 +23,11 @@ cd YCSB
 # Run the mcached benchmark
 mvn -pl site.ycsb:memcached-binding -am clean package
 ./bin/ycsb load memcached -s -P workloads/workloada -p "memcached.hosts=127.0.0.1" > /dev/null
-./bin/ycsb run memcached -s -P workloads/workloada -p "memcached.hosts=127.0.0.1" > ../workload_memcached_$1.txt
+./bin/ycsb run memcached -s -P workloads/workloada -p "memcached.hosts=127.0.0.1" > ../results/workload_memcached_$1.txt
 
 # Run the redis benchmark
 mvn -pl site.ycsb:redis-binding -am clean package
 ./bin/ycsb load redis -s -P workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" > /dev/null
-./bin/ycsb run redis -s -P workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" > ../workload_redis_$1.txt
+./bin/ycsb run redis -s -P workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" > ../results/workload_redis_$1.txt
 
 cd ..
--- run_microbenchmarks.sh
+++ run_microbenchmarks.sh
@@ -24,17 +24,14 @@ if [ "$#" -ne 1 ]; then
 fi
 
 # We only allow three kind of benchmark types
-if ! [[ "$1" == "board" || "$1" == "miralis" || "$1" == "prote" ]]; then
-    echo "Error: Invalid argument. Allowed values are 'board', 'miralis', or 'prote'."
+if ! [[ "$1" == "board" || "$1" == "miralis" || "$1" == "protect" ]]; then
+    echo "Error: Invalid argument. Allowed values are 'board', 'miralis', or 'protect'."
     exit 1
 fi
 
 echo "Benchmark type: $1"
 
 create_folder_if_not_exists "results"
-create_folder_if_not_exists "results/coremarkpro"
-create_folder_if_not_exists "results/iozone"
-create_folder_if_not_exists "results/netperf"
 
 ########################
 # CPU Microbenchmark
@@ -43,7 +40,7 @@ create_folder_if_not_exists "results/netperf"
 echo "Running CPU Microbenchmark [Coremarkpro]"
 
 cd coremark-pro
-make TARGET=linux64 XCMD='-c4' certify-all > "../results/coremarkpro/$1.txt"
+make TARGET=linux64 XCMD='-c4' certify-all > "../results/coremarkpro_$1.txt"
 cd ..;
 
 echo "Done with CPU microbenchmark"
@@ -55,7 +52,7 @@ echo "Done with CPU microbenchmark"
 echo "Running filesystem microbenchmark [Filesystem]"
 
 cd keystone-iozone
-../iozone -a > "../results/iozone/$1.txt"
+../iozone -a > "../results/iozone_$1.txt"
 cd ..
 
 echo "Done with disk microbenchmark"
@@ -71,11 +68,11 @@ cd netperf
 netserver
 # Benchmark the server
 echo "TCP microbenchmark"
-netperf -H 127.0.0.1 -t TCP_STREAM  > "../results/netperf/$1_tcp.txt"
+netperf -H 127.0.0.1 -t TCP_STREAM  > "../results/netperf/results_$1_tcp.txt"
 echo "UDP microbenchmark"
-netperf -H 127.0.0.1 -t UDP_STREAM  > "../results/netperf/$1_udp.txt"
+netperf -H 127.0.0.1 -t UDP_STREAM  > "../results/netperf/results_$1_udp.txt"
 echo "RTT microbenchmark"
-netperf -H 127.0.0.1 -t TCP_RR      > "../results/netperf/$1_rtt.txt"
+netperf -H 127.0.0.1 -t TCP_RR      > "../results/netperf/results_$1_rtt.txt"
 cd ..
 
 echo "Done with network microbenchmark"
