51f789cd434f05443c2b3872c85cb45251b82b98
François Costa <frankost.costa@gmail.com>
2024-12-09 14:02:42 +0100

    fixes in the scripts

--- coremark.py
+++ coremark.py
@@ -6,44 +6,57 @@ import numpy as np
 import io
 from plot import *
 
-# Function to parse Iozone output
 def parse_iozone_output(file_path):
     # Read the content of the file
     with open(file_path, "r") as file:
         text = file.read()
 
-    # Extract the relevant lines for the first table
-    lines = text.splitlines()
-    start = lines.index("| Workload Name            | MultiCore (iter/s) | SingleCore (iter/s) | Scaling |")
-    end = next(i for i, line in enumerate(lines[start + 1:], start=start + 1) if not line.strip().startswith("|"))
+    # Extract the WORKLOAD RESULTS TABLE
+    workload_start = text.find("WORKLOAD RESULTS TABLE")
+    workload_end = text.find("MARK RESULTS TABLE")
+    workload_section = text[workload_start+4:workload_end].strip()
 
-    # Join lines of the first table and convert to a DataFrame
-    table_text = "\n".join(lines[start:end])
-    df = pd.read_csv(io.StringIO(table_text), sep="|", skipinitialspace=True).drop(columns=["Unnamed: 0", "Unnamed: 5"])
-    # Drop the first ------- row
-    df = df.iloc[1:].reset_index(drop=True)
-    df.columns = df.columns.str.strip()
-    return df
+
+    # Process the workload table
+    workload_lines = workload_section.splitlines()[5:]
+
+    output = []
+    names = []
+    for line in workload_lines:
+        # Split the string into parts
+        parts = line.split()
+
+        names.append(parts[0])
+
+        # Iterate through parts to find the first number
+        for part in parts:
+            try:
+                output.append(float(part))
+                break
+            except ValueError:
+                continue
+    
+    return np.array(output), names
+
 
 
 if __name__ == "__main__":
     values = []
     names = []
+    indices = []
     folder_path = Path("results")
 
     for file_path in folder_path.rglob('*'): 
         if is_workload(file_path, "coremarkpro"):
             file_path = str(file_path)
-            df = parse_iozone_output(file_path)
             print(file_path.split('/')[1])
             names.append(file_path.split('/')[1].split('.')[0].split('_')[1])
 
-            values.append(df.set_index("Workload Name")["MultiCore (iter/s)"].astype(float))
+            val, index = parse_iozone_output(file_path)
+            values.append(val)
+            indices = index
 
-    
-    indices = np.array(values[0].index)
-    values = list(map(lambda x: x.values, values))
 
     title = 'Coremarkpro microbenchmark - [iterations/s] - multicore'
 
-    generate_plot(values, names, indices, title, "coremarkpro")
+    generate_plot(values, names, np.array(indices), title, "coremarkpro")
--- generate_plots.sh
+++ generate_plots.sh
@@ -10,11 +10,11 @@ python3 coremark.py
 echo "Plotting Disk Microbenchmark"
 python3 iozone.py
 
-echo "Plotting Network Microbenchmark"
-python3 netperf.py
+#echo "Plotting Network Microbenchmark"
+#python3 netperf.py
 
-echo "Plotting redis and memcached workloads"
-python3 kv_workload.py
+#echo "Plotting redis and memcached workloads"
+#python3 kv_workload.py
 
 
 
