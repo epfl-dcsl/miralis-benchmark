1949a837356c202e40befe5f4922692268785429
François Costa <frankost.costa@gmail.com>
2025-02-17 13:26:44 +0100

    Fix variance in MySQL benchmark

--- run_mysql.sh
+++ run_mysql.sh
@@ -19,7 +19,7 @@ WORKLOAD_NAME="mysql"
 sysbench \
      --db-driver=mysql   --mysql-host=$(echo "$ADDRESS" | cut -d'@' -f2-)   --mysql-port=3306 \
      --mysql-user=user   --mysql-password=user   --mysql-db=sbtest \
-     --tables=1   --table-size=100000   oltp_read_write   prepare
+     --tables=1   --table-size=10000   oltp_read_write   prepare
 
 # Clear previous file
 clear_stats_entries "${WORKLOAD_NAME}_$1"
@@ -28,7 +28,7 @@ add_miralis_stat_entry "${WORKLOAD_NAME}_$1"
 sysbench \
   --db-driver=mysql --mysql-host=$(echo "$ADDRESS" | cut -d'@' -f2-) --mysql-port=3306 \
   --mysql-user=user --mysql-password=user --mysql-db=sbtest \
-  --tables=1 --table-size=100000 --threads=32 \
+  --tables=1 --table-size=10000 --threads=128 \
   --time=300 --report-interval=20 oltp_read_write \
   run > "results/${WORKLOAD_NAME}_$1.txt"
 add_miralis_stat_entry "${WORKLOAD_NAME}_$1"
