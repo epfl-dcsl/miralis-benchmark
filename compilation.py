cca533aaa09b6986548f5315d6c00d21a658964c
François Costa <frankost.costa@gmail.com>
2025-02-20 17:43:10 +0100

    Add support for plotting the compilation duration of the linux kernel

--- compilation.py
+++ compilation.py
@@ -25,12 +25,30 @@ if __name__ == "__main__":
             workloads.append(extract_workload(file_path))
             iteration.append(extract_iteration(file_path))
 
-    print("coucou")
-
     title = 'Redis compilation multicore, in seconds'
 
     labels = ["real", "user", "sys"]
 
     generate_plot(values, workloads, labels, title, "redis_compilation")
+
+    values = []
+    names = []
+    iteration = []
+    folder_path = Path("results")
+
+    workloads = []
+
+    for file_path in sorted(folder_path.rglob('*')):
+        # Recursively search all files
+        if is_workload(file_path, "linux-compilation") and extract_iteration(file_path) == 0:
+            value = parse_times(file_path)
+            values.append(value)
+            workloads.append(extract_workload(file_path))
+            iteration.append(extract_iteration(file_path))
+
+    title = 'Linux kernel compilation multicore, in seconds'
+
+
+    generate_plot(values, workloads, labels, title, "linux_compilation")
 
 
