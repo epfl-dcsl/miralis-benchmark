fdd79ae5e1c627791a53ae2ca19036d4ac90355e
François Costa <frankost.costa@gmail.com>
2025-03-12 18:06:48 +0100

    Improvement in the boot figure

--- boot.py
+++ boot.py
@@ -12,7 +12,11 @@ import numpy as np
 
 import matplotlib.ticker as mticker
 
+csv = csv.drop('Firmware exit', axis=1)
+
 csv = csv.apply(lambda row: row / row.sum() if row.sum() > 0 else row, axis=1)
+
+
 
 data = {}
 for col in csv.columns:
@@ -26,8 +30,12 @@ ax.stackplot(unit_of_time, data.values(),
              labels=data.keys(), alpha=0.8)
 ax.legend(loc='upper left', reverse=True)
 ax.set_title('Proportion of exceptions per category')
-ax.set_xlabel('Unit of time')
-ax.set_ylabel('Cumulative percentage')
+ax.set_xlabel('1unit = 2 millions cpu cycles')
+ax.set_ylabel('Proportion')
+
+ax.set_ylim(0,1)
+ax.set_xlim(0,250)
+ax.axvline(x=113, color='black', linestyle='--')
 # add tick at every 200 million people
 ax.yaxis.set_minor_locator(mticker.MultipleLocator(.2))
 
